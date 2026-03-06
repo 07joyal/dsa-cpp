1256b666063cb4501f63b752ef5cb28c1372727b
Joyal <joyyy0016@gmail.com>
2026-03-06 23:09:58 +0530

    Add function to swap alternate values in array
    Implemented a program to swap alternate values in a 1-D array using a void function.

---
+++ SwapAlternateArray.cpp
@@ -0,0 +1,41 @@
+//program to swap alternate values in a 1-d array
+#include<bits/stdc++.h>
+using namespace std;
+
+//function to swap alternate values
+void swap(int array[], int i)
+{
+    int temp;
+    temp=array[i];
+    array[i]=array[i+1];
+    array[i+1]=temp;
+    
+}
+
+int main()
+{
+    int n;
+    cin>>n;//input array size
+    
+    int arr[n];
+    
+    for(int i=0;i<n;i++)//input array elements
+    {
+        cin >> arr[i];
+    }
+    
+    for(int i=0;i<n-1;i++)
+    {
+       
+         if(i%2 == 0)//swaps alternate indexes
+        {
+            swap(arr,i);//calling the function
+            }
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout << arr[i] << " ";//printing the swapped array values
+    }
+    return 0;
+    
+}
